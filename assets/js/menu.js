6775709799f13e4aa0949db7a0fc5310240e1918
Rafael Roberto de Oliveira <70959880+Fel1324@users.noreply.github.com>
2022-12-07 13:03:29 -0300

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+function openMenu(){
+    document.querySelector('aside.menu').classList.add('menu-open');
+}
+
+function closeMenu(){
+    document.querySelector('aside.menu').classList.remove('menu-open');
+}
