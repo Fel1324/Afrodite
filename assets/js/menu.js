d610ab3c6e22a14e7135c5805a56eb1a373fa5b9
Rafael Roberto de Oliveira <70959880+Fel1324@users.noreply.github.com>
2023-01-01 18:44:09 -0300

    Aprimoramentos do menu-lateral

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,9 @@
 function openMenu(){
     document.querySelector('aside.menu').classList.add('menu-open');
+    document.body.style.overflow = 'hidden';
 }
 
 function closeMenu(){
     document.querySelector('aside.menu').classList.remove('menu-open');
+    document.body.style.overflow = 'auto';
 }
